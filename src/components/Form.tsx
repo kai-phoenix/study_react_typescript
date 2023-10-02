50b007aef0c47044372264a6475320705a37976b
kaito <zax04097@outlook.jp>
2023-10-03 00:25:09 +0900

    form_api

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,16 @@
 //Form.tsx
 import { useState } from "react";
+
 const Form =() =>{
     const [city,setCity]=useState<string>();
+    const getWeather =(e:any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=9f07072fb2644ee4927151038230210&q=London&aqi=no").then(res =>res.json()).then(data => console.log(data))
+    }
     return (
         <form>
             <input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)}/>
-            {city}
-            <button type="submit">Get Weather</button>
+            <button type="submit" onClick={getWeather}>Get Weather</button>
         </form>
     );
 };
