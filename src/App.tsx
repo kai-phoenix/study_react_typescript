35db6f7d55731555a70bae30ada954c017db1a16
kaito <zax04097@outlook.jp>
2023-10-09 23:18:06 +0900

    result.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
 import Title from "./components/Title";
 import Form from "./components/Form"
+import Results from "./components/Results";
 import './App.css';
+
 function App() {
+  const [city,setCity]=useState<string>("");
+    const getWeather =(e:any) => {
+        e.preventDefault();
+        fetch("http://api.weatherapi.com/v1/current.json?key=9f07072fb2644ee4927151038230210&q=London&aqi=no").then(res =>res.json()).then(data => console.log(data))
+    }
   return (
     <div className="test">
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather}/>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,16 +1,13 @@
 //Form.tsx
-import { useState } from "react";
-
-const Form =() =>{
-    const [city,setCity]=useState<string>();
-    const getWeather =(e:any) => {
-        e.preventDefault();
-        fetch("http://api.weatherapi.com/v1/current.json?key=9f07072fb2644ee4927151038230210&q=London&aqi=no").then(res =>res.json()).then(data => console.log(data))
-    }
+type FormPropsType={
+    setCity:React.Dispatch<React.SetStateAction<string>>;
+    getWeather:(e:any)=>void;
+}
+const Form =(props:FormPropsType) =>{
     return (
         <form>
-            <input type="text" name="city" placeholder="都市名" onChange={e =>setCity(e.target.value)}/>
-            <button type="submit" onClick={getWeather}>Get Weather</button>
+            <input type="text" name="city" placeholder="都市名" onChange={e =>props.setCity(e.target.value)}/>
+            <button type="submit" onClick={props.getWeather}>Get Weather</button>
         </form>
     );
 };
