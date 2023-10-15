ce1ae789903c272919c047ba4528663bdfff4a26
kaito <zax04097@outlook.jp>
2023-10-16 00:29:05 +0900

    result.tsx2

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,39 @@ import Form from "./components/Form"
 import Results from "./components/Results";
 import './App.css';
 
+type ResultStateType={
+  country:string;
+  cityName:string;
+  temperature:string;
+  conditionText:string;
+  icon:string;
+}
 function App() {
   const [city,setCity]=useState<string>("");
+  const [results,setResults]=useState<ResultStateType>({
+    country:"",
+    cityName:"",
+    temperature:"",
+    conditionText:"",
+    icon:""
+  });
     const getWeather =(e:any) => {
         e.preventDefault();
-        fetch("http://api.weatherapi.com/v1/current.json?key=9f07072fb2644ee4927151038230210&q=London&aqi=no").then(res =>res.json()).then(data => console.log(data))
+        fetch("http://api.weatherapi.com/v1/current.json?key=9f07072fb2644ee4927151038230210&q=London&aqi=no").then(res =>res.json()).then(data => {
+          setResults({
+            country: data.location.country,
+            cityName:data.location.name ,
+            temperature: data.current.temp_c,
+            conditionText:data.current.condition.text ,
+            icon:data.current.condition.icon
+          })
+        })
     }
   return (
     <div className="test">
       <Title />
       <Form setCity={setCity} getWeather={getWeather}/>
+      <Results results={results}/>
     </div>
   );
 }
--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,6 +1,19 @@
-const Results=() =>{
+type ResultsPropsType={
+    results:{
+        country:string;
+        cityName:string;
+        temperature:string;
+        conditionText:string;
+        icon:string;
+    }
+}
+const Results=(props:ResultsPropsType) =>{
     return(
-        <h1>気象データ</h1>
+        // <h1>気象データ{2<4 && <p>正しいですか？</p>}{props.results.country}</h1>
+        <div>
+            {props.results.country&&
+            <div>{props.results.country}</div>}
+        </div>
     );
 };
 
